eea9e8912f5dcfe2883a1a59e5882ac489c0ce73
Darren Hill <dchill42@gmail.com>
2014-01-02 16:25:24 -0500

    Fixed Xylophone license header
    Signed-off-by: Darren Hill <dchill42@gmail.com>

--- system/core/Xylophone.php
+++ system/core/Xylophone.php
@@ -10,8 +10,8 @@
  * Licensed under the Open Software License version 3.0
  *
  * This source file is subject to the Open Software License (OSL 3.0) that is
- * bundled with this package in the files license.txt / license.rst. It is also
- * available through the world wide web at this URL:
+ * bundled with this package in the files license.txt / license.rst. It is
+ * also available through the world wide web at this URL:
  * http://opensource.org/licenses/OSL-3.0
  * If you did not receive a copy of the license and are unable to obtain it
  * through the world wide web, please send an email to licensing@xylophone.io
@@ -19,7 +19,7 @@
  *
  * @package     Xylophone
  * @author      Xylophone Dev Team, EllisLab Dev Team
- * @copyright   Copyright (c) 2008 - 2013, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright   Copyright (c) 2014, Xylophone Team (http://xylophone.io/)
  * @license     http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * @link        http://xylophone.io
  * @since       Version 1.0
