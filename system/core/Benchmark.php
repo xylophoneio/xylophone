e8a2a0a91a8291f776ea73190d46fd837e7dde8c
Darren Hill <dchill42@gmail.com>
2014-01-02 16:30:22 -0500

    Converted Benchmark to Xylophone
    Signed-off-by: Darren Hill <dchill42@gmail.com>

--- system/core/Benchmark.php
+++ system/core/Benchmark.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * CodeIgniter
+ * Xylophone
  *
- * An open source application development framework for PHP 5.2.4 or newer
+ * An open source HMVC application development framework for PHP 5.3 or newer
+ * Derived from CodeIgniter, Copyright (c) 2008 - 2013, EllisLab, Inc. (http://ellislab.com/)
  *
  * NOTICE OF LICENSE
  *
@@ -13,17 +14,19 @@
  * also available through the world wide web at this URL:
  * http://opensource.org/licenses/OSL-3.0
  * If you did not receive a copy of the license and are unable to obtain it
- * through the world wide web, please send an email to
- * licensing@ellislab.com so we can send you a copy immediately.
+ * through the world wide web, please send an email to licensing@xylophone.io
+ * so we can send you a copy immediately.
  *
- * @package		CodeIgniter
- * @author		EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2013, EllisLab, Inc. (http://ellislab.com/)
- * @license		http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
- * @link		http://codeigniter.com
- * @since		Version 1.0
+ * @package     Xylophone
+ * @author      Xylophone Dev Team, EllisLab Dev Team
+ * @copyright   Copyright (c) 2014, Xylophone Team (http://xylophone.io/)
+ * @license     http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link        http://xylophone.io
+ * @since       Version 1.0
  * @filesource
  */
+namespace Xylophone\core;
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
@@ -32,94 +35,68 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * This class enables you to mark points and calculate the time difference
  * between them. Memory consumption can also be displayed.
  *
- * @package		CodeIgniter
- * @subpackage	Libraries
- * @category	Libraries
- * @author		EllisLab Dev Team
- * @link		http://codeigniter.com/user_guide/libraries/benchmark.html
+ * @package     Xylophone
+ * @subpackage  core
+ * @author      Xylophone Dev Team
+ * @link        http://xylophone.io/user_guide/libraries/benchmark.html
  */
-class CI_Benchmark {
+class Benchmark
+{
+    /** @var    array   List of all benchmark markers */
+    public $marker = array();
 
-	/**
-	 * List of all benchmark markers
-	 *
-	 * @var	array
-	 */
-	public $marker = array();
+    /**
+     * Set a benchmark marker
+     *
+     * Multiple calls to this function can be made so that several
+     * execution points can be timed.
+     *
+     * @param   string  Marker name
+     * @return  void
+     */
+    public function mark($name)
+    {
+        $this->marker[$name] = microtime(true);
+    }
 
-	/**
-	 * Set a benchmark marker
-	 *
-	 * Multiple calls to this function can be made so that several
-	 * execution points can be timed.
-	 *
-	 * @param	string	$name	Marker name
-	 * @return	void
-	 */
-	public function mark($name)
-	{
-		$this->marker[$name] = microtime(TRUE);
-	}
+    /**
+     * Elapsed time
+     *
+     * Calculates the time difference between two marked points.
+     *
+     * If the first parameter is empty this function instead returns the
+     * {elapsed_time} pseudo-variable. This permits the full system
+     * execution time to be shown in a template. The output class will
+     * swap the real value for this variable.
+     *
+     * @param   string  A particular marked point
+     * @param   string  A particular marked point
+     * @param   int     Number of decimal places
+     * @return  string  Calculated elapsed time on success, an '{elapsed_string}'
+     *                  if $point1 is empty or an empty string if $point1 is not found.
+     */
+    public function elapsedTime($point1 = '', $point2 = '', $decimals = 4)
+    {
+        ($point1 !== '') || return '{elapsed_time}';
+        isset($this->marker[$point1]) || return '';
+        isset($this->marker[$point2]) || $this->marker[$point2] = microtime(true);
+        return number_format($this->marker[$point2] - $this->marker[$point1], $decimals);
+    }
 
-	// --------------------------------------------------------------------
-
-	/**
-	 * Elapsed time
-	 *
-	 * Calculates the time difference between two marked points.
-	 *
-	 * If the first parameter is empty this function instead returns the
-	 * {elapsed_time} pseudo-variable. This permits the full system
-	 * execution time to be shown in a template. The output class will
-	 * swap the real value for this variable.
-	 *
-	 * @param	string	$point1		A particular marked point
-	 * @param	string	$point2		A particular marked point
-	 * @param	int	$decimals	Number of decimal places
-	 *
-	 * @return	string	Calculated elapsed time on success,
-	 *			an '{elapsed_string}' if $point1 is empty
-	 *			or an empty string if $point1 is not found.
-	 */
-	public function elapsed_time($point1 = '', $point2 = '', $decimals = 4)
-	{
-		if ($point1 === '')
-		{
-			return '{elapsed_time}';
-		}
-
-		if ( ! isset($this->marker[$point1]))
-		{
-			return '';
-		}
-
-		if ( ! isset($this->marker[$point2]))
-		{
-			$this->marker[$point2] = microtime(TRUE);
-		}
-
-		return number_format($this->marker[$point2] - $this->marker[$point1], $decimals);
-	}
-
-	// --------------------------------------------------------------------
-
-	/**
-	 * Memory Usage
-	 *
-	 * Simply returns the {memory_usage} marker.
-	 *
-	 * This permits it to be put it anywhere in a template
-	 * without the memory being calculated until the end.
-	 * The output class will swap the real value for this variable.
-	 *
-	 * @return	string	'{memory_usage}'
-	 */
-	public function memory_usage()
-	{
-		return '{memory_usage}';
-	}
-
+    /**
+     * Memory Usage
+     *
+     * Simply returns the {memory_usage} marker.
+     *
+     * This permits it to be put it anywhere in a template
+     * without the memory being calculated until the end.
+     * The output class will swap the real value for this variable.
+     *
+     * @return  string  '{memory_usage}'
+     */
+    public function memoryUsage()
+    {
+        return '{memory_usage}';
+    }
 }
 
-/* End of file Benchmark.php */
-/* Location: ./system/core/Benchmark.php */
